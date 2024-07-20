8e17f393a1e6a426cfc798623eb4175125eb6389
vasiliyvn <mishadasha1114@gmail.com>
2024-07-20 21:37:06 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+my_string=input('You name?: ')
+print(len(my_string),'')
+print('my_string'.upper())
+print('my_string'.lower())
+print('my_string'.replace('_',''))
+print('my_string'[0])
+print('my_string'[-1])
+
+
+
